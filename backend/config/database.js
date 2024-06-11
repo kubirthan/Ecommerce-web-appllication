2ed98e3e9857df3eb45d8b084b41ecdac47a26bf
kubirthan <kubirthan7991@gmail.com>
2024-06-11 21:15:28 +0530

    database connected

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose')
 
+ const connectDatabase = ()=> {
+    mongoose.connect(process.env.DB_LOCAL_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }).then(con=>{
+        console.log(`mongodb is connected to the host: ${con.connection.host}`);
+    }).catch((err)=>{
+        console.log(err);
+    })
+ }
+
+ module.exports = connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const app = require('./app')
 const dotenv = require('dotenv')
 const path = require('path')
+const connectDatabase = require('./config/database')
 
 dotenv.config({path:path.join(__dirname,"config/config.env")})
+
+connectDatabase()
 
 app.listen(process.env.PORT, ()=>{
     console.log(`server listining to the port ${process.env.PORT} in ${process.env.NODE_ENV}`);
