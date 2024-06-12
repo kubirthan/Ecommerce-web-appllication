8ad046fcfda8db968160322de007166159ffd175
kubirthan <kubirthan7991@gmail.com>
2024-06-12 15:28:50 +0530

    product model is created

--- backend/controllers/productControlller.js
+++ backend/controllers/productControlller.js
@@ -1,6 +1,16 @@
+const Product = require('../models/productModel')
+
 exports.getProducts = (req,res,next) => {
     res.status(200).json({
         success:true,
         message: "this route will show all the products in database"
+    })
+}
+
+exports.newProduct = async (req,res,next) => {
+    const product = await Product.create(req.body)
+    res.status(201).json({
+        success: true,
+        product
     })
 }
