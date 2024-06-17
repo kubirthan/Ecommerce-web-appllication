97918518f2570a829795b7698ef7985c28bd693a
kubirthan <kubirthan7991@gmail.com>
2024-06-17 18:04:12 +0530

    delete product

--- backend/controllers/productControlller.js
+++ backend/controllers/productControlller.js
@@ -60,3 +60,24 @@ exports.updateProduct = async (req,res,next)=>{
         product
       })
 }
+
+
+//delete product - /api/v1/product:id
+exports.deleteProduct = async (req,res,next)=>{
+    const product = Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "product not found"
+        })
+    }
+
+    await product.deleteOne()
+
+    res.status(200).json({
+        success: true,
+        message: "Product deleted"
+    })
+
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productControlller')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productControlller')
 const router = express.Router()
 
 router.route('/products').get(getProducts)
@@ -7,6 +7,7 @@ router.route('/product/new').post(newProduct)
 router.route('/product/:id')
                             .get(getSingleProduct)
                             .put(updateProduct)
+                            .delete(deleteProduct)
 
-                            
+
 module.exports = router
