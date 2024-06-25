ce763752627e9f18a64311e884dbef1ef9736fd9
kubirthan <kubirthan7991@gmail.com>
2024-06-25 22:01:21 +0530

    Sending Token in Cookie

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -4,7 +4,15 @@ const sendToken = (user, statusCode, res ) => {
     //creating jwt token
     const token = user.getJwtToken()
 
-    res.status(statusCode).json({
+    //setting cookies
+    const options = {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 7 * 24*60*60*1000),
+        httpOnly: true
+    }
+
+    res.status(statusCode)
+    .cookie('token', token, options)
+    .json({
         success: true,
         token,
         user
