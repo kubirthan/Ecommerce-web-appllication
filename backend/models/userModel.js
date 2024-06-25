9caa3e2195cb92c0ce32c3f36878dff15279d8bf
kubirthan <kubirthan7991@gmail.com>
2024-06-25 09:38:15 +0530

    Hashing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -35,6 +36,10 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     }
+})
+
+userSchema.pre('save', async function(next){
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 let model = mongoose.model('user', userSchema)
