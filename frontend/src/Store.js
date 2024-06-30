89608a1f5101aa13cca8b8435e57022cbe9cf0e2
kubirthan <kubirthan7991@gmail.com>
2024-06-30 12:29:19 +0530

    created products reducer

---
+++ frontend/src/Store.js
@@ -0,0 +1,16 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {thunk} from 'redux-thunk';
+import productsReducer from './slices/ProductsSlice'
+
+
+const reducer = combineReducers({
+    productsState: productsReducer
+})
+
+
+const store = configureStore({
+    reducer
+})
+
+
+export default store
