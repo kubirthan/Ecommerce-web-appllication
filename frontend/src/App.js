739d2d7bff7445e0e8e6bb2024eb8e19967c6e11
kubirthan <kubirthan7991@gmail.com>
2024-07-18 22:01:01 +0530

    frontend payment integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import ProductDetail from './components/product/ProductDetail';
 import ProductSearch from './components/product/ProductSearch';
 import Login from './components/user/Login';
 import Register from './components/user/Register';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import store from './Store'
 import { loadUser } from './actions/userActions';
 import Profile from './components/user/Profile';
@@ -23,11 +23,19 @@ import ResetPassword from './components/user/ResetPassword';
 import Cart from './components/cart/Cart';
 import Shipping from './components/cart/Shipping';
 import ConfirmOrder from './components/cart/ConfirmOrder';
+import Payment from './components/cart/Payment';
+import axios from 'axios';
+import {Elements} from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js';
 
 function App() {
-
+  const [stripeApiKey, setStripeApiKey] = useState("")
   useEffect(()=> {
     store.dispatch(loadUser)
+    async function getStripeApiKey() {
+      const {data} = await axios.get('/api/v1/stripeapi')
+     setStripeApiKey(data.stripeApiKey)
+    }
   })
 
 
@@ -52,6 +60,7 @@ function App() {
             <Route path='/cart' element={<Cart/>} />
             <Route path='/shipping' element={<ProtectedRoute><Shipping/></ProtectedRoute>} />
             <Route path='/order/confirm' element={<ProtectedRoute><ConfirmOrder/></ProtectedRoute>} />
+            <Route path='/payment' element={<ProtectedRoute><Elements stripe={loadStripe(stripeApiKey)}><Payment/></Elements></ProtectedRoute>} />
           </Routes>
            </div>
           <Footer/>
