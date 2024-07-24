63b166814cf6484ff7560364f678a14027d775b8
kubirthan <kubirthan7991@gmail.com>
2024-07-24 20:58:50 +0530

    user orde4r actions

--- frontend/src/slices/orderSlice.js
+++ frontend/src/slices/orderSlice.js
@@ -32,9 +32,131 @@ const orderSlice = createSlice({
                 loading: false,
                 error: action.payload
             }
+        },
+        clearError(state, action) {
+            return {
+                ...state,
+                error: null
+            }
+        },
+        userOrdersRequest(state, action) {
+            return {
+                ...state,
+                loading: true
+            }
+        },
+        userOrdersSuccess(state, action) {
+            return {
+                ...state,
+                loading: false,
+                userOrders: action.payload.orders
+            }
+        },
+        userOrdersFail(state, action) {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        },
+        orderDetailRequest(state, action) {
+            return {
+                ...state,
+                loading: true
+            }
+        },
+        orderDetailSuccess(state, action) {
+            return {
+                ...state,
+                loading: false,
+                orderDetail: action.payload.order
+            }
+        },
+        orderDetailFail(state, action) {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        },
+        adminOrdersRequest(state, action) {
+            return {
+                ...state,
+                loading: true
+            }
+        },
+        adminOrdersSuccess(state, action) {
+            return {
+                ...state,
+                loading: false,
+                adminOrders: action.payload.orders
+            }
+        },
+        adminOrdersFail(state, action) {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        },
+
+        deleteOrderRequest(state, action) {
+            return {
+                ...state,
+                loading: true
+            }
+        },
+        deleteOrderSuccess(state, action) {
+            return {
+                ...state,
+                loading: false,
+                isOrderDeleted: true
+            }
+        },
+        deleteOrderFail(state, action) {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        },
+
+        updateOrderRequest(state, action) {
+            return {
+                ...state,
+                loading: true
+            }
+        },
+        updateOrderSuccess(state, action) {
+            return {
+                ...state,
+                loading: false,
+                isOrderUpdated: true
+            }
+        },
+        updateOrderFail(state, action) {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        },
+
+        clearOrderDeleted(state, action) {
+            return {
+                ...state,
+                isOrderDeleted: false
+            }
+        },
+        clearOrderUpdated(state, action) {
+            return {
+                ...state,
+                isOrderUpdated: false
+            }
         }
+
     }
-})
+});
 
 const { actions, reducer } = orderSlice;
 
